e78b98250637ca028eb53b0b1ae6ccf7a7b12b81
Björn Sletten <sletten93@live.se>
2019-10-24 18:23:02 +0200

    Massa vue av Adam

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+//.use(VueAxios, axios)
+//Vue.prototype.$http = axios
+//Vue.prototype.$axios = axios
+window.axios = require('axios');
---
+++ assets/vue/app.vue
@@ -0,0 +1,27 @@
+var app = new Vue({
+
+
+
+
+  el: '#submitButton',
+
+  data: {
+
+    url: 'HEJHEJ',
+    temp: '',
+  },
+
+  methods: {
+
+    serverCall: function() {
+
+      temp = document.getElementById('additionOptions');
+      this.url = temp.options[temp.selectedIndex].value;
+      console.log(this.url);
+      this.url += document.getElementById('val1').value;
+
+      //axios.post(this.url);
+
+    }
+  }
+})
