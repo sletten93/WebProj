ca9519d9b7b45016fdf9f3b15ec0f0fa33c8be45
Björn Sletten <sletten93@live.se>
2019-10-30 12:34:48 +0100

    app.vue förändringar

--- assets/vue/app.vue
+++ assets/vue/app.vue
@@ -1,24 +1,38 @@
+// <script>
 var app = new Vue({
 
-  el: '#submitButton',
+  el: '#app',
 
   data: {
 
     url: 'HEJHEJ',
-    temp: '',
+    selected: "selected",
+    key: ""
+    // optgroup: ''
   },
 
   methods: {
 
     serverCall: function() {
+      this.url = "api/" + this.selected;
 
-      temp = document.getElementById('additionOptions');
-      this.url = "/api/" + temp.options[temp.selectedIndex].value;
       this.url += document.getElementById('val1').value;
       console.log('ADDING: ' + this.url);
+      // $http.post('http://sportstatsweb.jls-sto1.elastx.net/');
+      // axios.post('http://sportstatsweb.jls-sto1.elastx.net/' + this.url);
 
-      //axios.post('http://sportstatsweb.jls-sto1.elastx.net/' + this.url);
+      var redirectWindow = window.open('http://sportstatsweb.jls-sto1.elastx.net/' + this.url);
+      redirectWindow.location;
 
+    },
+
+    optClick: function optClick(event) {
+      console.log(this.key);
+      // this.selected = event.target.value;
+      // console.log(this.selected);
     }
+
+
+
   }
 })
